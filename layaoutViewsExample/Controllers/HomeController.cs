cb977c8d7a981e6fbd0308473d53cbb8e0f703c1
Raidy <raidi16150905@gmail.com>
2026-04-29 14:23:49 -0400

    Add project files.

---
+++ layaoutViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace layaoutViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/about/us")]
+        public IActionResult about() 
+        {
+            return View();
+        }
+
+        [Route("/contact/support")]
+        public IActionResult contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ layaoutViewsExample/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace layaoutViewsExample.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+
+        [Route("Order/Products")]
+        public IActionResult Search()
+        {
+            return View();
+        }
+
+        [Route("Search/Products")]
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
